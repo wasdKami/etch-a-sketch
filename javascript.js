41b8811a0fc629158d47ca382544a0b2be7d450b
WasdKami <110654379+wasdKami@users.noreply.github.com>
2023-09-23 22:58:32 +0200

    Update javascript.js
    I am putting it straight into github since i am tired

--- javascript.js
+++ javascript.js
@@ -1,50 +1,107 @@
 const gridContainer = document.querySelector(".gridContainer");
-let horizontalInput = document.querySelector(".horizontalInput");
-let verticalInput = document.querySelector(".verticalInput");
+const inputContainer = document.querySelector(".inputContainer");
+const cell = document.querySelectorAll(".cell");
 
-let horizontal;
-let vertical;
+const gridValue = document.querySelector(".gridValue");
+const speedValue = document.querySelector(".speedValue");
+const timeoutToggle = document.querySelector("#timeoutToggle")
 
-horizontalInput.addEventListener('input', function () {
+let scaleInput = document.querySelector(".scaleInput");
+let timeoutSpeed = 1000;
+let currentColor = 'black';
+let toggle = false;
+
+gridValue.textContent = 0;
+speedValue.textContent = timeoutSpeed;
+
+let horizontal = 0;
+let vertical = 0;
+
+//---------------------------------------------------------------------------------------------
+//Get input 
+
+scaleInput.addEventListener('input', function () {
 	// As a number
-	horizontal = horizontalInput.valueAsNumber;
-    console.log("horizontal: " + horizontal);
+	scale = scaleInput.valueAsNumber;
+    //console.log("vertical: " + vertical);
 });
 
-verticalInput.addEventListener('input', function () {
-	// As a number
-	vertical = verticalInput.valueAsNumber;
-    console.log("vertical: " + vertical);
-});
-
+//---------------------------------------------------------------------------------------------
+//when button is pressed create column and row
 
 function submit(){
-    console.log("horizontal and vertical Input: " + horizontal + " " + vertical);
-    createGrid(horizontal, vertical);
-}
-
-function createGrid(width, height){
-    let childElements = document.querySelectorAll(".gridCell");
+    let childElements = document.querySelectorAll(".column");
     for (let i = childElements.length - 1; i >= 0; i--) {
         gridContainer.removeChild(childElements[i]);
     }
-    
-    let cellAmount = width * height;
-    console.log(cellAmount);
-    for(let i = 0; i < cellAmount; i++){
-        const gridCell = document.createElement('div');
+    //console.log("horizontal and vertical Input: " + horizontal + " " + vertical);
+    createGrid(scale);
+    gridValue.textContent = scale;
+}
 
-        gridCell.classList.add('gridCell');
-        gridCell.style.width = '100px';
-        gridCell.style.height = '100px';
-        gridCell.style.backgroundColor = randomColor();
+function quickSubmit(value){
+    let childElements = document.querySelectorAll(".column");
+    for (let i = childElements.length - 1; i >= 0; i--) {
+        gridContainer.removeChild(childElements[i]);
+    }
+    createGrid(value);
+    gridValue.textContent = value;
+}
 
-        gridContainer.appendChild(gridCell);
+//---------------------------------------------------------------------------------------------
+// create column
+
+function createGrid(cell){
+    for(let i = 0; i < cell; i++){
+        const column = document.createElement("div");
+        column.classList.add('column');
+        gridContainer.appendChild(column);
+
+        for(let i = 0; i < cell; i++){
+            const cell = document.createElement("div");
+            cell.classList.add('cell');
+            cell.style.backgroundColor = 'black';
+            column.appendChild(cell);
+
+            timeoutToggle.addEventListener('change', function () {
+                if (timeoutToggle.checked == true) {
+                    toggle = true;
+                } else {
+                    toggle = false;
+                }
+            });
+
+            if(timeoutToggle.checked){
+                cell.addEventListener('mouseenter', () => {                    
+                    cell.style.backgroundColor = currentColor;
+                });
+            } else {
+                cell.addEventListener('mouseenter', () => {                    
+                    cell.style.backgroundColor = currentColor;
+                    setTimeout(() => {
+                        cell.style.backgroundColor = 'black';
+                    }, timeoutSpeed);
+                });
+            }
+        }
     }
 }
+
+//---------------------------------------------------------------------------------------------
+// Choose random color
 
 function randomColor(){
     const picks = ['yellow', 'red', 'orange', 'pink', 'blue', 'purple'];
     let choice = Math.floor(Math.random() * picks.length);
     return picks[choice];
+}
+
+function timeout(amount){
+    timeoutSpeed = amount;
+    console.log(timeoutSpeed)
+    speedValue.textContent = timeoutSpeed;
+}
+
+function changeColor(color){
+    currentColor = color
 }
